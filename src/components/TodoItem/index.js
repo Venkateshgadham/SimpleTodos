852292668f34fe654fa66ee4909c24918bbe364d
Venkateshgadham <venkateshgadham456@gmail.com>
2025-11-12 03:42:23 +0000

    first commit

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,21 @@
+import './index.css'
+
+const TodoItem = props => {
+  const {todoDetails, onDeleteTodo} = props
+  const {id, title} = todoDetails
+
+  const handleDelete = () => {
+    onDeleteTodo(id)
+  }
+
+  return (
+    <li className="todo-item">
+      <p className="todo-title">{title}</p>
+      <button type="button" className="delete-button" onClick={handleDelete}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
